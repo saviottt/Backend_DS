cbee5f1588d33a25a9e7257150e48f40a91c081f
Don Savio Thomas <donsavio1one@gmail.com>
2026-02-16 18:16:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+<<<<<<< HEAD
+import joblib
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Load model
+model = joblib.load("sales_forecasting_model.pkl")
+model_features = joblib.load("model_features.pkl")
+
+# Define input structure
+class SalesInput(BaseModel):
+    Item_Weight: float
+    Item_Fat_Content: str
+    Item_Visibility: float
+    Item_Type: str
+    Item_MRP: float
+    Outlet_Size: str
+    Outlet_Location_Type: str
+    Outlet_Type: str
+    Outlet_Age: int
+
+
+@app.post("/predict")
+def predict(data: SalesInput):
+    df = pd.DataFrame([data.dict()])
+    df = pd.get_dummies(df)
+    df = df.reindex(columns=model_features, fill_value=0)
+
+    prediction = model.predict(df)
+
+    return {"predicted_sales": float(prediction[0])}
+=======
+import joblib
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Load model
+model = joblib.load("sales_forecasting_model.pkl")
+model_features = joblib.load("model_features.pkl")
+
+# Define input structure
+class SalesInput(BaseModel):
+    Item_Weight: float
+    Item_Fat_Content: str
+    Item_Visibility: float
+    Item_Type: str
+    Item_MRP: float
+    Outlet_Size: str
+    Outlet_Location_Type: str
+    Outlet_Type: str
+    Outlet_Age: int
+
+
+@app.post("/predict")
+def predict(data: SalesInput):
+    df = pd.DataFrame([data.dict()])
+    df = pd.get_dummies(df)
+    df = df.reindex(columns=model_features, fill_value=0)
+
+    prediction = model.predict(df)
+
+    return {"predicted_sales": float(prediction[0])}
+>>>>>>> 296fe8a (Initial commit)
